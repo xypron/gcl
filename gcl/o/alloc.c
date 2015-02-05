d92f6d53c1e34eb15b7b513f3cd16af0813c5ba6
Camm Maguire <camm@debian.org>
2015-02-05 15:44:58 -0500

    turn on relocatable_buffer

--- gcl/o/alloc.c
+++ gcl/o/alloc.c
@@ -524,14 +524,19 @@ alloc_from_freelist(struct typemanager *tm,fixnum n) {
     break;
 
   default:
-    if (tm==tm_table+t_cons && sSAstatic_relocatable_bufferA && sSAstatic_relocatable_bufferA->s.s_dbind!=Cnil) {
-      if (sSAstatic_relocatable_bufferA->s.s_dbind->v.v_fillp==sSAstatic_relocatable_bufferA->s.s_dbind->v.v_dim/2)
-	return NULL;
-      p=sSAstatic_relocatable_bufferA->s.s_dbind->v.v_self+sSAstatic_relocatable_bufferA->s.s_dbind->v.v_fillp;
-      sSAstatic_relocatable_bufferA->s.s_dbind->v.v_fillp+=sizeof(struct cons)/sizeof(object);
-      fprintf(stderr,"Allocating relocable buffer %p\n",p);
-      fflush(stderr);
-      return p;
+    if (tm==tm_table+t_cons && sSAstatic_relocatable_bufferA) {
+      if (sSAstatic_relocatable_bufferA->s.s_dbind==Cnil) {
+      	sSAstatic_relocatable_bufferA->s.s_dbind=(VFUN_NARGS=4,FFN(fSmake_vector1)(make_fixnum(40000),fSget_aelttype(sLfixnum),Ct,make_fixnum(0)));
+      }
+      if (sSAstatic_relocatable_bufferA->s.s_dbind!=Cnil) {
+	if (sSAstatic_relocatable_bufferA->s.s_dbind->v.v_fillp==sSAstatic_relocatable_bufferA->s.s_dbind->v.v_dim/2)
+	  return NULL;
+	p=sSAstatic_relocatable_bufferA->s.s_dbind->v.v_self+sSAstatic_relocatable_bufferA->s.s_dbind->v.v_fillp;
+	sSAstatic_relocatable_bufferA->s.s_dbind->v.v_fillp+=sizeof(struct cons)/sizeof(object);
+	fprintf(stderr,"Allocating relocable buffer %p\n",p);
+	fflush(stderr);
+	return p;
+      }
     }
     if ((p=tm->tm_free)!=OBJNULL) {
       tm->tm_free = OBJ_LINK(p);
